e56fd74beb197b789640209a75c626502a112906
Михаил Макашин <makashyn@Mihails-MacBook-Pro.local>
2019-05-12 23:50:00 +0300

    created react app

---
+++ frontend/booking/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>Some</div>
+    );
+  }
+}
+
+export default App;
